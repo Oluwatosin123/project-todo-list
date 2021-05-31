a957e986779447e9227702ec134d69b50b5df42b
Ajose Tosin <tosinemidastouch@gmail.com>
2021-05-31 16:08:48 +0100

    first commit

--- app.js
+++ app.js
@@ -12,60 +12,80 @@ filterOption.addEventListener('click', filterTodo)
 
 // Functions
 function addTodo(event) {
-// prevent form from submitting
-event.preventDefault();
+    // prevent form from submitting
+    event.preventDefault();
 
-// Create Todo Div
-const todoDiv = document.createElement("div");
-todoDiv.classList.add("todo");
+    // Create Todo Div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
 
-// Create LI
-const newTodo = document.createElement("li");
-newTodo.innerText = todoInput.value;
-newTodo.classList.add('todo-item');
-todoDiv.appendChild(newTodo);
+    // Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
 
-// CHECK MARK BUTTON
-const completedButton = document.createElement('button');
-completedButton.innerHTML = '<i class="fas fa-check"></i>';
-completedButton.classList.add("complete-btn");
-todoDiv.appendChild(completedButton);
+    // CHECK MARK BUTTON
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
 
-// CHECK MARK BUTTON
-const trashButton = document.createElement('button');
-trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-trashButton.classList.add("trash-btn");
-todoDiv.appendChild(trashButton);
+    // CHECK MARK BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
 
-// APPEND TO LIST
-todoList.appendChild(todoDiv)
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv)
 
-// CLEAR INPUT FIELD
-todoInput.value = "";
+    // CLEAR INPUT FIELD
+    todoInput.value = "";
 };
 
 
-function deleteCheck(e){
+function deleteCheck(e) {
     const item = e.target;
     // DELETE  TODO
-    if (item.classList[0] ==="trash-btn") {
+    if (item.classList[0] === "trash-btn") {
         const todo = item.parentElement;
         // Animation
         todo.classList.add('fall');
-       todo.addEventListener('transitionend', function(){
-           todo.remove()
-       })
+        todo.addEventListener('transitionend', function () {
+            todo.remove()
+        })
     };
 
     // CHECK MARK
-    if(item.classList[0] === "complete-btn"){
-const todo = item.parentElement;
-todo.classList.toggle('completed')
+    if (item.classList[0] === "complete-btn") {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed')
     }
 };
 
-function filterTodo(e){
+function filterTodo(e) {
     const todos = todoList.childNodes;
-    console.log(todos);
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todos.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = "flex"
+                } else {
+                    todo.style.display = "none"
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = "flex"
+                } else {
+                    todo.style.display = "none"
+                }
+                break;
+        }
+    })
 }
 
